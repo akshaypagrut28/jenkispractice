af2216c9f34000cdb9bf30c4d155ad9ff9fa47ac
Admin <Admin@DESKTOP-77FO7BT>
2022-09-15 12:48:16 +0530

    commit and push by 6th time updated

--- src/test/java/jenkinspractice/ReadDataFromCmdLineTest.java
+++ src/test/java/jenkinspractice/ReadDataFromCmdLineTest.java
@@ -1,5 +1,23 @@
 package jenkinspractice;
 
-public class ReadDataFromCmdLineTest {
+import org.testng.annotations.Test;
 
-}
+public class ReadDataFromCmdLineTest {
+	
+
+		@Test
+		public void readDataTest() {
+			
+			String BROWSER = System.getProperty("browser");
+			String URL = System.getProperty("url");
+			String USERNAME = System.getProperty("username");
+			String PASSWORD = System.getProperty("password");
+			
+			System.out.println(BROWSER);
+			System.out.println(URL);
+			System.out.println(USERNAME);
+			System.out.println(PASSWORD);
+		}
+	}
+
+
